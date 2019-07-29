72e2952cc531a7aff2497e391e2402064b3e661a
Nicole Dow <nikki.dow71@gmail.com>
2019-07-29 21:54:35 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist
+  attr_accessor :name
+  @@all = []  
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_song(song)
+    song.artist = self
+    self.songs << song
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.all.detect {|artist| artist.name == name}
+      self.all.detect {|artist| artist.name == name}
+    else
+      new_artist = self.new(name)
+      new_artist
+    end
+  end
+  
+  def print_songs(artist)
+    self.songs do |song|
+      puts song
+    end
+  end
+  
+end
