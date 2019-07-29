8ea32a2eb4be7be5ea40b2029a4277068608dd59
Nicole Dow <nikki.dow71@gmail.com>
2019-07-29 22:06:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,9 @@ class Song
   #   new_song
   # end
   
-  def 
+  def self.new_by_filename(filename)
+    
+  end
   
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
